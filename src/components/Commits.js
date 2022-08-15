5c58401fae6e28f848a1f2983b88b2e77c125373
joandino <68410777+joandino@users.noreply.github.com>
2022-08-15 01:38:48 -0600

    New styling for view the commits

--- src/components/Commits.js
+++ src/components/Commits.js
@@ -1,5 +1,15 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from "axios";
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+import CommitIcon from '@mui/icons-material/Commit';
+const _ = require("underscore");
 
 export default function Commits() {
     const [commits, setCommits] = useState([]);
@@ -13,6 +23,8 @@ export default function Commits() {
     }, [commits]);
 
     const getCommits = async () => {
+        setCommits([]);
+        
         const res = await axios.get('https://api.github.com/repos/joandino/commit-viewer/commits', {
             headers: {
                 'Authorization': `token ${process.env.REACT_APP_GITHUB_PERSONAL_TOKEN}` 
@@ -25,11 +37,31 @@ export default function Commits() {
     }
 
     const renderCommits = () => {
-        console.log(commits);
-        const mappedCommits = commits.map((item, index) => (
-            <li key={index}>
-                <span>{item.commit.message}</span>
-            </li>
+        const alldates = _.groupBy(commits, function(obj) {
+            const date = new Date(obj.commit.author.date).toISOString().slice(0, 10);
+            return date;
+        });
+
+        const mappedCommits = Object.entries(alldates)
+        .map(([key, value]) => (
+            <Step active={true} key={key}>
+                <StepLabel StepIconComponent={CommitIcon}>
+                    {`Commits on ${new Date(key).toDateString()}`}
+                </StepLabel>
+                <StepContent>
+                    {value.map((item, index) => (
+                        <Card sx={{ maxWidth: "100%" }} key={index}>
+                            <CardHeader
+                                avatar={
+                                    <Avatar src={item.author.avatar_url} />
+                                }
+                                title={item.commit.message}
+                                subheader={item.commit.author.name}
+                            />
+                        </Card>
+                    ))}
+                </StepContent>
+            </Step>
         ));
 
         return mappedCommits;
@@ -37,7 +69,11 @@ export default function Commits() {
 
     return (
         <Fragment>
-            <ul>{renderCommits()}</ul>
+            <Box className="container">
+                <Stepper orientation="vertical">
+                    {renderCommits()}
+                </Stepper>
+            </Box>
         </Fragment>
     )
 }
